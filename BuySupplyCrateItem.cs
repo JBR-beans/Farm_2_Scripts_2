ad3c73cb378551fd1ca4b02b3d109289c0503dc5
RustyBeans <107262647+RustyBeans@users.noreply.github.com>
2024-09-15 17:56:01 -0700

    added supply crate

---
+++ BuySupplyCrateItem.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class BuySupplyCrateItem : UdonSharpBehaviour
+{
+    public GameObject _supplyCrate;
+    public GameObject _PS1;
+    public GameObject _PS2;
+    public void ActivateSupplyCrate()
+    {
+        _PS1.SetActive(false);
+        _PS2.SetActive(false);
+        _supplyCrate.SetActive(true);
+    }
+}
---
+++ SupplyDropHitGround.cs
@@ -0,0 +1,21 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class SupplyDropHitGround : UdonSharpBehaviour
+{
+	public GameObject _Item;
+	public GameObject _PS1;
+	public GameObject _PS2;
+	public void OnCollisionEnter(Collision collision)
+	{
+		_PS1.transform.transform.position = transform.position;
+		_PS2.transform.transform.position = transform.position;
+		_Item.SetActive(true);
+		_PS1.SetActive(true);
+		_PS2.SetActive(true);
+		this.gameObject.SetActive(false);
+	}
+}
